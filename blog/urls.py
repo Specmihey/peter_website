5fcf5c59c9dc5908e63317584cc0a6766a8e8fa4
Mikhail Chizhov <specmihey@gmail.com>
2024-11-30 23:36:28 +0300

    Меню, акт ссылки, карта, include html

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,14 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
+    path('doctors/', doctors, name='doctors'),
+    path('about-clinic/', about_clinic, name='about-clinic'), #о клинике
+    path('price/', price, name='price'), #price
+    path('faq/', faq, name='faq'), #FAQ
+    path('service/', service, name='service'), #service
+    path('time_table/', time_table, name='time_table'), #time_table
+    path('contact/', contact, name='contact'), # contact
+    path('privacy_policy/', privacy_policy, name='privacy_policy'), #privacy_policy
     # path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,82 @@ from django.shortcuts import render, redirect, get_object_or_404
 # Create your views here.
 def index(request):
    context={
-       'title': 'Digital Dental Art',
+       'title': 'Стоматологическая клиника Digital Dental Art',
        'description': 'Стоматологическая клиника Digital Dental Art',
 
    }
    return render(request, 'blog/index.html', context=context)
+
+def doctors(request):
+   context={
+       'title': 'Наши врачи',
+       'super_title': 'Познакомьтесь с нашими квалифицированными врачами',
+       'description': 'Лучшие врачи клиники Digital Dental Art',
+
+   }
+   return render(request, 'blog/doctors.html', context=context)
+
+def about_clinic(request):
+   context={
+       'title': 'О клинике',
+       'super_title': 'О нас',
+       'description': 'Стоматологическая клиника Digital Dental Art',
+
+   }
+   return render(request, 'blog/about_clinic.html', context=context)
+
+def price(request):
+   context={
+       'title': 'Прайс',
+       'super_title': 'Наши цены',
+       'description': 'Стоматологическая клиника Digital Dental Art',
+
+   }
+   return render(request, 'blog/price.html', context=context)
+
+
+def faq(request):
+   context={
+       'title': 'Frequently Asked Questions',
+       'super_title': 'Часто задаваемые вопросы',
+       'description': 'Стоматологическая клиника Digital Dental Art',
+
+   }
+   return render(request, 'blog/faq.html', context=context)
+
+def service(request):
+   context={
+       'title': 'Услуги',
+       'super_title': 'Услуги клиники',
+       'description': 'Стоматологическая клиника Digital Dental Art',
+
+   }
+   return render(request, 'blog/service.html', context=context)
+
+def time_table(request):
+   context={
+       'title': 'Время работы',
+       'super_title': 'Время работы клиники',
+       'description': 'Время работы стоматологической клиники Digital Dental Art',
+
+   }
+   return render(request, 'blog/time_table.html', context=context)
+
+def privacy_policy(request):
+   context={
+       'title': 'Политика конфиденциальности',
+       'super_title': 'Privacy Policy',
+       'description': 'Privacy Policy стоматологической клиники Digital Dental Art',
+
+   }
+   return render(request, 'blog/privacy_policy.html', context=context)
+
+def contact(request):
+   context={
+       'title': 'Контакты клиники',
+       'super_title': 'Как нас найти',
+       'description': 'Контакты стоматологической клиники Digital Dental Art',
+
+   }
+   return render(request, 'blog/contact.html', context=context)
+
